440ce94ee2b1111b509ca67fdc2dd236f68df548
mincms <68103403@qq.com>
2013-06-13 21:13:03 +0800

    SiteController

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -4,7 +4,7 @@ use app\core\FrontController;
 use app\models\LoginForm;
 use app\models\ContactForm;
 use app\vendor\Geo;
-use yii\helpers\FileHelper;
+ 
  
 class SiteController extends FrontController
 { 
@@ -18,9 +18,7 @@ class SiteController extends FrontController
 	}
  
 	public function actionIndex()
-	{    
-		new FileHelper;
-		exit;
+	{     
 		$this->active = 'site.index';  
 		echo $this->render('index');
 	} 
